7d355673a5cc61d3abf0e9cec9123282a6a08686
omerokumus <omerokumus3@gmail.com>
2022-03-09 21:06:34 +0300

    finalizing the app

--- app/src/main/java/com/example/mycalculator_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator_kotlin/MainActivity.kt
@@ -25,12 +25,105 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun onDigit(view: View){
+    fun onEqual(view: View){
+        var startsWithMinus = false
+        if (inputTextView?.text != null){
+            if (entryContainsOperator() && isLastEntryDigit()) {
+                var inputString = inputTextView?.text.toString()
+                if (inputString.startsWith("-")) {
+                    inputString = inputString.substring(1)
+                    startsWithMinus = true
+                }
+                val operator = getOperator(inputString)
+                val inputSplit = inputString.split(operator)
+                val firstPart = getPrefix(startsWithMinus) + inputSplit[0]
+                val secondPart = inputSplit[1]
+                inputTextView?.text = when (operator){
+                    "+" -> {
+                        (firstPart.toDouble() + secondPart.toDouble()).toString()
+                    }
+                    "-" -> {
+                        (firstPart.toDouble() - secondPart.toDouble()).toString()
+                    }
+                    "*" -> {
+                        (firstPart.toDouble() * secondPart.toDouble()).toString()
+                    }
+                    "/" -> {
+                        if (secondPart.toInt() == 0) "DivByZero" else (firstPart.toDouble() / secondPart.toDouble()).toString()
+                    }
+                    else -> {""}
+                }
+            }
+        }
+
+    }
+
+    private fun getPrefix(startsWithMinus: Boolean): String {
+        return if (startsWithMinus) {
+            "-"
+        }
+        else {
+            ""
+        }
+    }
+
+    private fun getOperator(str: String): String {
+        return if (str.contains("+")){
+            "+"
+        } else if (str.contains("-")){
+            "-"
+        } else if (str.contains("*")){
+            "*"
+        } else
+            "/"
+    }
+
+    fun onDigit(view: View) {
+        handleDivByZero()
         inputTextView?.append((view as Button).text)
     }
 
-    fun onClear(view: View){
+    fun onClear(view: View) {
         inputTextView?.text = ""
+    }
+
+    fun onDotClicked(view: View) {
+        handleDivByZero()
+        if (inputTextView?.text?.contains('.') == false)
+            inputTextView?.append(".")
+    }
+
+    private fun handleDivByZero() {
+        if(inputTextView?.text?.equals("DivByZero") == true)
+            inputTextView?.text = ""
+    }
+
+    fun onOperator(view: View) {
+        handleDivByZero()
+        if (inputTextView?.text?.isEmpty() == true && (view as Button).text == "-")
+            inputTextView?.append(view.text)
+
+        else if (isLastEntryDigit() && !entryContainsOperator())
+            inputTextView?.append((view as Button).text)
+    }
+
+    private fun entryContainsOperator(): Boolean {
+        inputTextView?.text?.let {
+            if (it.isEmpty())
+                return false
+            val substr = it.substring(1)
+            return substr.contains('*') || substr.contains('/')
+                    || substr.contains('+') || substr.contains('-')
+        }
+
+        return false
+    }
+
+    private fun isLastEntryDigit(): Boolean {
+        inputTextView?.let {
+            return it.text.last().isDigit()
+        }
+        return false
     }
 
 //    private fun addOnClickListenersTo(buttons: MutableList<Button>) {
